b5a8285bfc435003755cc18d01c6ed241d7cc8cb
andreykoleber <andrei.koleber@gmail.com>
2018-11-13 22:45:37 +0600

    begin to do the work from scratch again

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-/*global console, require*/
-
 var http = require('http');
-var fs = require('fs');
-var extract = require('./extract');
-var handleError = function (err, res) {
-  'use strict';
-  res.writeHead(404);
-  res.end();
-};
 
 var server = http.createServer(function (req, res) {
-  'use strict';
-  console.log('Responding to a request.');
-
-  var filePath = extract(req.url);
-  fs.readFile(filePath, function (err, data) {
-    if (err) {
-      handleError(err, res);
-      return;
-    } else {
-      res.setHeader('Content-Type', 'text/html');
-      res.end(data);
-    }
-  });
+  console.log('Responding to a request');
+  res.end('<h1>Hello, World!!');
 });
 server.listen(3000);
