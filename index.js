4ab5bb0a81e42fff44d02c886b14246fa5911280
andreykoleber <andrei.koleber@gmail.com>
2018-11-15 21:44:51 +0600

    Add custom module

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
 
 var server = http.createServer(function (req, res) {
   console.log('Responding to a request');
-  res.end('<h1>Hello, World!!');
+
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function (err, data) {
+    res.end(data);
+  });
 });
 server.listen(3000);
