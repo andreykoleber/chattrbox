6211e13e7be571f6e75d0b680875221c9218c1b4
andreykoleber <andrei.koleber@gmail.com>
2018-12-01 14:50:50 +0600

    Add chat functionality and avatars

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,16 +1,37 @@
 import socket from './ws-client';
+import {
+  ChatForm,
+  ChatList,
+  promptForUsername
+} from './dom';
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]';
+const LIST_SELECTOR = '[data-chat="message-list"]';
+
+let username = '';
+username = promptForUsername();
 
 class ChatApp {
   constructor() {
+    this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+    this.chatList = new ChatList(LIST_SELECTOR, username);
+
     socket.init('ws://localhost:3001');
     socket.registerOpenHandler(() => {
-      let message = new ChatMessage({
-        message: 'pow!'
+      this.chatForm.init((data) => {
+        let message = new ChatMessage({
+          message: data
+        });
+        socket.sendMessage(message.serialize());
       });
-      socket.sendMessage(message.serialize());
     });
+
     socket.registerMessageHandler((data) => {
       console.log(data);
+      let message = new ChatMessage(data);
+      this.chatList.drawMessage(message.serialize());
+      console.log(message);
     });
   }
 }
@@ -18,7 +39,7 @@ class ChatApp {
 class ChatMessage {
   constructor({
     message: m,
-    user: u = 'batman',
+    user: u = username,
     timestamp: t = (new Date()).getTime()
   }) {
     this.message = m;
